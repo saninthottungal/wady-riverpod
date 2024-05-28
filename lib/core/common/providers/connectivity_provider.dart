29c18a65b47b2a2a5c5acc4a22fade062a3cb60e
sanin <saninthottungalhere@gmail.con>
2024-05-28 22:37:20 +0530

    connectivity provider added

---
+++ lib/core/common/providers/connectivity_provider.dart
@@ -0,0 +1,9 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final connectivityInstanceProvider = Provider((ref) => Connectivity());
+final networkProvider =
+    StreamProvider.autoDispose<List<ConnectivityResult>>((ref) {
+  final instance = ref.watch(connectivityInstanceProvider);
+  return instance.onConnectivityChanged;
+});
