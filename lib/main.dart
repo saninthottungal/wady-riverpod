90349aafdacbfe1b6c936dde083ee2ccf6928985
sanin <saninthottungalhere@gmail.con>
2024-05-29 22:42:34 +0530

    adapters registered

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:weather_riverpod/common/providers/shared_preferences_provider.dart';
 import 'package:weather_riverpod/core/enums/theme_enum.dart';
 import 'package:weather_riverpod/core/themes/providers/theme_provider.dart';
 import 'package:weather_riverpod/core/themes/theme.dart';
+import 'package:weather_riverpod/models/city_entity/city_entity.dart';
+import 'package:weather_riverpod/models/weather_entity/weather_entity.dart';
 import 'package:weather_riverpod/screens/home/screen_home.dart';
 import 'package:weather_riverpod/screens/search/screen_search.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  if (!Hive.isAdapterRegistered(1)) {
+    Hive.registerAdapter(WeatherEntityAdapter());
+  }
+  if (!Hive.isAdapterRegistered(2)) {
+    Hive.registerAdapter(CityEntityAdapter());
+  }
+
   runApp(
     const ProviderScope(
       child: MyApp(),
